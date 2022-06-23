4d2f8cc30de6d1076f61c7a70a1082369b76fb8d
Hadi Albinsaad <hey@hadi.gg>
2022-06-23 15:36:40 +0300

    CHnage hosyt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { server } from './server';
 
-server.listen({ port: 3000 }).catch((err) => {
-	server.log.error(err);
-	process.exit(1);
-});
+server
+	.listen({
+		port: 3001,
+		host: '0.0.0.0',
+	})
+	.catch((err) => {
+		server.log.error(err);
+		process.exit(1);
+	});
